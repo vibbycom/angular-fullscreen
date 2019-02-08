60bf0cfbc751987351cd2c68b417c6c2222d394b
Luka Vidakovic <luka@vibby.com>
2019-02-08 11:17:51 +0100

    Fix fullscreen for Chrome >= 71
    Issue and fix are described here: https://github.com/fabiobiondi/angular-fullscreen issue 61

--- src/angular-fullscreen.js
+++ src/angular-fullscreen.js
@@ -5,9 +5,6 @@
       module.factory('Fullscreen', ['$document', '$rootScope', function ($document,$rootScope) {
          var document = $document[0];
 
-         // ensure ALLOW_KEYBOARD_INPUT is available and enabled
-         var isKeyboardAvailbleOnFullScreen = (typeof Element !== 'undefined' && 'ALLOW_KEYBOARD_INPUT' in Element) && Element.ALLOW_KEYBOARD_INPUT;
-
          var emitter = $rootScope.$new();
 
          // listen event on document instead of element to avoid firefox limitation
@@ -27,12 +24,7 @@
                } else if(element.mozRequestFullScreen) {
                   element.mozRequestFullScreen();
                } else if(element.webkitRequestFullscreen) {
-                  // Safari temporary fix
-                  if (/Version\/[\d]{1,2}(\.[\d]{1,2}){1}(\.(\d){1,2}){0,1} Safari/.test(navigator.userAgent)) {
-                     element.webkitRequestFullscreen();
-                  } else {
-                     element.webkitRequestFullscreen(isKeyboardAvailbleOnFullScreen);
-                  }
+                  element.webkitRequestFullscreen();
                } else if (element.msRequestFullscreen) {
                   element.msRequestFullscreen();
                }
